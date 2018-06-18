fb268f581e102af7d827074bc4395e3e1779a9bd
Greg Kaleka <greg.kaleka@gmail.com>
2018-06-18 09:08:11 -0700

    Python3 compatability changes

--- gib_detect.py
+++ gib_detect.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+from __future__ import print_function
 
 import pickle
 import gib_detect_train
@@ -6,7 +7,7 @@ import gib_detect_train
 model_data = pickle.load(open('gib_model.pki', 'rb'))
 
 while True:
-    l = raw_input()
+    l = input()
     model_mat = model_data['mat']
     threshold = model_data['thresh']
-    print gib_detect_train.avg_transition_prob(l, model_mat) > threshold
+    print(gib_detect_train.avg_transition_prob(l, model_mat) > threshold)
