5f554671517b8d4ebb4ca6a88218f853d199b0e7
Timofei Semenov <timasem@replika.ai>
2019-10-08 12:38:48 +0300

    Fix paths

--- gibberish/config.py
+++ gibberish/config.py
@@ -4,5 +4,6 @@ import string
 ACCEPTED_CHARS = string.ascii_lowercase + ' '
 CHAR_TO_IDX = {char: idx for idx, char in enumerate(ACCEPTED_CHARS)}
 
-DATA_DIR = 'data'
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
 MODEL_FILE_PATH = os.path.join(DATA_DIR, 'model.pkl')
